0940260c2af815a32f50f55c020aab0780c5375f
chris <chris.j.field1992@gmail.com>
2017-09-07 18:56:55 +0100

    typing helpers

--- Client/src/helpers/apiHelper.ts
+++ Client/src/helpers/apiHelper.ts
@@ -1,12 +1,13 @@
+import { customApiHeaders } from '../interfaces/apiInterfaces';
 import baseURL from '../appConfig';
 
 class APIHelper {
-    static apiCall(method : string, endpoint : string, body? : object, urlParams? : string) {
-        const customheaders = {
+    static apiCall(method: string, endpoint: string, body?: object, urlParams?: string) {
+        const customheaders: customApiHeaders = {
             'Content-Type': 'application/json;charset=UTF-8',
         };
-        const headers = new Headers(customheaders);
-        let calledUrl = `${baseURL}${endpoint}`;
+        const headers: Headers = new Headers(customheaders);
+        let calledUrl: string = `${baseURL}${endpoint}`;
   
         if (urlParams) {
             calledUrl += `?${urlParams}`;
@@ -18,19 +19,17 @@ class APIHelper {
             mode: 'cors',
             body: body ? JSON.stringify(body) : undefined,
         })
-        .then((response : any) => APIHelper.checkStatus(response))
-        .catch((error : any) => {
-            throw(error);
-        });
+            .then((response: any) => APIHelper.checkStatus(response))
+            .catch((error: any) => {throw(error);});
     }
   
-    static checkStatus(response : any) {
+    static checkStatus(response: any) {
         if (response.ok && response.status === 204) {
             return true;
         } else if (response.ok) {
             return response.json();
         } else {
-            const error = new Error(response.statusText);
+            const error: Error = new Error(response.statusText);
             throw error;
         }
     }
---
+++ Client/src/interfaces/apiInterfaces.ts
@@ -0,0 +1,4 @@
+export interface customApiHeaders {
+    'Content-Type': String;
+
+}
